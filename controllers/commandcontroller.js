4fd5560b9e70d9f24ee9ce9fc77d328943af3571
Narihito <heinboss234@gmail.com>
2026-02-22 12:25:15 +0630

    .

--- controllers/commandcontroller.js
+++ controllers/commandcontroller.js
@@ -1,10 +1,10 @@
-export const command = (bot) => (msg) => {
+export const command = (bot) => async (msg) => {
     const chatid = msg.chat.id;
     const message = msg.text;
     console.log(message);
 
     if(message === "/start"){
-        bot.sendMessage(chatid, "You can now get started! This is your personal ai assistance that can help you with anything. Develop By Narihito")
+       await bot.sendMessage(chatid, "You can now get started! This is your personal ai assistance that can help you with anything. Develop By Narihito")
     }
-    
+
 }
