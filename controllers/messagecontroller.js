f0d53f3d90fd85b388451cc4dc434a0d08058be6
Narihito <heinboss234@gmail.com>
2026-02-25 09:15:46 +0630

    added safety setting

--- controllers/messagecontroller.js
+++ controllers/messagecontroller.js
@@ -148,6 +148,10 @@ export const image = (bot) => async (msg) => {
                 numberOfImages: 1,
                 aspectRatio: "1:1"
             },
+            safetySettings: [
+                { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_ONLY_HIGH" },
+                { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_ONLY_HIGH" }
+            ]
         })
 
         const imageBytes = imageresponse?.generatedImages?.[0]?.image?.imageBytes;
