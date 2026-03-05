23d92f60640abd29823784133ae2884c6b618ece
Narihito <heinboss234@gmail.com>
2026-03-05 15:12:09 +0630

    .

--- controllers/videocontroller.js
+++ controllers/videocontroller.js
@@ -12,7 +12,7 @@ export const Video = (bot) => async (msg) => {
             let result = await Gemini.models.generateVideos({
                 model: model,
                 source: {
-                    prompt: `${message}. Generate Only 4 seconds video.`
+                    prompt: `${message}. Generate Only 1 seconds video.`
                 },
                 config: {
                     numberOfVideos: 1,
@@ -32,9 +32,11 @@ export const Video = (bot) => async (msg) => {
             const buffer = Videodata.videoBytes;
 
             await bot.sendVideo(chatid, buffer, {
+                caption: "Here is the video you requested!"
+            }, {
                 filename: 'video.mp4',
                 contentType: 'video/mp4'
-            })
+            });
 
             await Gemini.files.delete({
                 name: Videodata
@@ -48,5 +50,4 @@ export const Video = (bot) => async (msg) => {
         console.log(err);
         await bot.sendMessage(chatid, "It seems something went wrong");
     }
-
 }
