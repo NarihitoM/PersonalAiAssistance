31edf24653c3156174abc4e989f6a476cb496482
NarihitoM <heinboss234@gmail.com>
2026-09-14 08:35:55 +0630

    chore: remove unused usersession model
    - not imported anywhere in the codebase

--- src/models/usersession.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const Usersession = new mongoose.Schema({
-    userid : { type : String },
-    session : {type : String, default : "chat"}
-})
-
-export default mongoose.model("Usersession",Usersession);
-
